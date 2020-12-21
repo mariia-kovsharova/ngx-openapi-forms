a25faf5c4af5bb2e767847b6295ea87b294f2f17
fiorsaoirse <solovyanovamaria@yandex.ru>
2020-12-21 11:40:13 +0300

    Linter configs fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,26 @@
 module.exports = {
-  extends: ['plugin:@typescript-eslint/recommended'],
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-  env: {
-    "browser": true,
-    "es2021": true
-  },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    "ecmaVersion": 12,
-    "sourceType": "module",
-    "project": "./tsconfig.json",
-  },
-  plugins: [
-    "@typescript-eslint"
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
   ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    project: 'tsconfig.json',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: false,
+    },
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  env: {
+    node: true,
+    browser: true,
+  },
+  rules: {},
 };
