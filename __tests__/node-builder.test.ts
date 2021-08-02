78b577ab2e101c0003ecd39fb4bf00b1426ea62f
fiorsaoirse <solovyanovamaria@yandex.ru>
2021-08-02 13:31:14 +0300

    tests + node builder refactor start

---
+++ __tests__/node-builder.test.ts
@@ -0,0 +1,28 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+import buildNode from '../src/services/node-builder';
+
+const FILES = ['nested', 'cat'];
+const FIXTURES = path.join(__dirname, '__fixtures__');
+const NORMALIZED_FOLDER = path.join(FIXTURES, 'normalized');
+
+describe('Normalize function', () => {
+    // FILES.forEach((name: string) => {
+    //     const fileName = `${name}.json`;
+
+    //     it(`Normalization of file: "${fileName}"`, async () => {
+    //         const dataToProcessFilePath = path.join(INITIAL_FOLDER, fileName);
+    //         const normalizedDataFilePath = path.join(NORMALIZED_FOLDER, fileName);
+
+    //         const dataToProcessContent = await fs.readFile(dataToProcessFilePath, 'utf-8');
+    //         const normalizedDataContent = await fs.readFile(normalizedDataFilePath, 'utf-8');
+
+    //         const [dataToProcess] = Object.values(JSON.parse(dataToProcessContent));
+    //         const [normalizedData] = Object.values(JSON.parse(normalizedDataContent));
+
+    //         const processedData = normalize(dataToProcess as any);
+
+    //         expect(processedData).toEqual(normalizedData);
+    //     })
+    // })
+});
